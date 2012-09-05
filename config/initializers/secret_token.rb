3a7b74018ec9fdd1dbc236737887f79c03442545
Shrikrushna <shrikrushna2107@gmail.com>
2012-09-06 03:59:22 +0530

    Listing of Books

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopping::Application.config.secret_token = '0599362d7b2fd6903b391750653a97451439cdf92533b23f7e66fb4a35ae56419a61c5ec96a5cfa13edf7e5c0a8bf1c2e923079b805735c71eeeb851fa2da9a3'
