a8b3f9dd4ba1278681f2194e8f775fe23a4d9471
Shrikrushna <shrikrushna2107@gmail.com>
2012-09-08 01:59:39 +0530

    css modified

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,11 @@
 class Order < ActiveRecord::Base
    attr_accessible :address, :email, :name
-  # ...
+
    validates :name, :address, :email, :presence => true
   
 
   has_many :line_items, :dependent => :destroy
-  # ...
-  # ...
+  
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
