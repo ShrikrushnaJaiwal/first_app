b2573ebf28c33c5961730e73484a9d65515c6f67
Shrikrushna <shrikrushna2107@gmail.com>
2012-09-08 14:22:34 +0530

    Admin access

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Shopping::Application.routes.draw do
+  get "admin/index"
+
+ # get "sessions/new"
+
+ 
+# get "sessions/create"
+
+ # get "sessions/destroy"
+   
+  get 'admin' => 'admin#index'
+  controller :sessions do
+	get 'login' => :new
+	post 'login' => :create
+	delete 'logout' => :destroy
+  end
+ 
   resources :users
 
   resources :orders
@@ -63,7 +79,7 @@ Shopping::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'store#index', :as => 'store'
+  root :to => 'store#index'#, :as => 'store'
 
 
   # See how all your routes lay out with "rake routes"
